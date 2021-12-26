a67a835db5b6faf4a1e3cae42e7ded6c5f9bd0d0
Isaia <2000newmar@gmail.com>
2021-12-25 22:05:16 -0700

    returns interesting index page @ /count

--- challenge4/main.py
+++ challenge4/main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 # make a server that serves an index.html file from http://localhost:1234/count
 
 
+@app.route('/forwardslash')
+def forwardslash():
+    return 'we are hot'
+
+
 @app.route('/count')
-def index():
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
